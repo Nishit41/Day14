8770f8e055c9a9e9c5027d77f3d420acf48098cf
Nishit <nishitranjan41@gmail.com>
2022-07-26 22:28:01 +0530

    this is welcome of linked list

---
+++ src/com/bridgelabz/datastructure/LinkedListMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.datastructure;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.push(70);
+        linkedList1.push(30);
+        linkedList1.push(56);
+        linkedList1.print();
+        System.out.println();
+      //  System.out.println(linkedList1.pop());
+        //System.out.println();
+        //linkedList1.print();
+    }
+}
