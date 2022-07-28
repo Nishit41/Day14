832c7d7cce46e20c12f88c1fcfbcd9b7c4610919
Nishit41 <106942237+Nishit41@users.noreply.github.com>
2022-07-28 21:41:48 +0530

    Update Node.java

--- src/com/bridgelabz/datastructure/Node.java
+++ src/com/bridgelabz/datastructure/Node.java
@@ -4,10 +4,9 @@ public class Node<T> {
     T data;
     Node<T> next;
 
-    public Node() {
-    }
 
-    public Node(T data) {
+    public Node(T data) 
+    {        
         this.data = data;
     }
 }
