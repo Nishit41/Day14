9dfde3d221dbe697d203aa8f8db9d25e2f0ecdb9
Nishit <nishitranjan41@gmail.com>
2022-07-29 10:35:01 +0530

    this is my stack file

---
+++ src/com/bridgelabz/stack/Stacks.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.stack;
+
+
+import com.bridgelabz.datastructure.LinkedList;
+
+    public class Stacks <T>{
+        LinkedList<T> linkedList;
+        public Stacks(){
+            this.linkedList=new LinkedList<>();
+        }
+
+        void push(T data){
+            linkedList.push(data);
+        }
+
+        void display(){
+            linkedList.print();
+        }
+
+        void pop(){
+            linkedList.pop();
+        }
+
+        public static void main(String[] args) {
+            Stacks<Integer> stack = new Stacks<>();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.display();
+        }
+    }
+
